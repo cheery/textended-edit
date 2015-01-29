db5422ff7d50b8bd3eda9891b83e22e46de5b7d6
Henri Tuhola <cheery@boxbase.org>
2015-01-29 18:50:48 +0200

    added explicit voice

--- print_speak.py
+++ print_speak.py
@@ -1,24 +1,27 @@
 import textended
 import sys
+import pyttsx
+
+engine = pyttsx.init()
+engine.setProperty('rate', 70)
+voices = engine.getProperty('voices')
 
 contents = textended.load(open(sys.argv[1]))
 
 def print_out(node):
-    if node.label != "":
-        sys.stdout.write(node.label)
     if node.contents is None:
         pass
     elif isinstance(node.contents, str):
-        sys.stdout.write(':binary ' + node.contents.encode('hex') + ';')
+        engine.say(node.label + " binary" + node.contents.encode('hex'))
     elif isinstance(node.contents, unicode):
-        sys.stdout.write(':string ' + node.contents + ';')
+        engine.say(node.label + " string " + node.contents)
     else:
-        sys.stdout.write('[ ')
+        engine.say(node.label + " begin")
         for subnode in node:
             print_out(subnode)
-        sys.stdout.write(']')
-    sys.stdout.write(' ')
+        engine.say("done")
 
 for node in contents:
     print_out(node)
-sys.stdout.write('\n')
+
+engine.runAndWait()
